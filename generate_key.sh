475987040b2a0a7dd74f63388da3641785124db5
germaniumsculk <germaniumsculk@gmail.com>
2024-10-13 20:25:34 +0600

    Update generate_key.sh

--- generate_key.sh
+++ generate_key.sh
@@ -3,7 +3,7 @@ function keygen() {
     [ -z "$1" ] || certs_dir=$1
     rm -rf "$certs_dir"
     mkdir -p "$certs_dir"
-    local subject = "/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=germaniumsculk@gmail.com"    
+    local subject="/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=germaniumsculk@gmail.com"    
     for key in bluetooth certs cyngn-app media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do
         printf "\n\n" | ./development/tools/make_key "$certs_dir"/$key "$subject"
     done
