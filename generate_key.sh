6152d1c9b40b86e12139b896a2a43923caa47e90
germaniumsculk <germaniumsculk@gmail.com>
2024-10-13 17:53:06 +0600

    Update generate_key.sh

--- generate_key.sh
+++ generate_key.sh
@@ -3,16 +3,9 @@ function keygen() {
     [ -z "$1" ] || certs_dir=$1
     rm -rf "$certs_dir"
     mkdir -p "$certs_dir"
-    local subject
-    echo "Sample subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
-    echo "Now enter subject details for your keys:"
-    for entry in C ST L O OU CN emailAddress; do
-        echo -n "$entry:"
-        read -r val
-        subject+="/$entry=$val"
-    done
+    local subject = "/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=germaniumsculk@gmail.com"    
     for key in bluetooth certs cyngn-app media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do
-        ./development/tools/make_key "$certs_dir"/$key "$subject"
+        printf "\n\n" | ./development/tools/make_key "$certs_dir"/$key "$subject"
     done
 }
 keygen
